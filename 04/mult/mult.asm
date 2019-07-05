cdfc03741c951b9843a5217155c40c0d6bea1c09
discocorg <david.flaten@gmail.com>
2019-07-05 13:50:52 -0700

    completed multiplication assembly program

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,40 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2    //Initialize Sum to zero
+M=0
+@R1    //Getting second digit to be used as a counter for the loop
+D=M
+@ZERO  //Jump to Zero if R1 Zero
+D;JEQ
+@R3    //Storing counter
+M=D
+@R1    //checking first digit for zero
+D=M
+@ZERO  //Jump to Zero if R1 Zero
+D;JEQ
+(LOOP)
+@R0
+D=M    // D=R0
+@R2
+M=D+M  // D=R2+R0
+@R3    // Reducing Counter
+D=M
+@END   // Checking for zero
+D;JEQ
+@R3
+M=M-1
+D=M
+@END
+D;JEQ
+@LOOP
+0;JMP  // Goto LOOP
+@ZERO
+@R2
+M=0
+D=M
+@END   // Checking for zero
+D;JEQ
+(END)
+@END
+0;JMP  // Infinite loop
