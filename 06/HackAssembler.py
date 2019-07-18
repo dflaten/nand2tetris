7e196f96006f339a8829ef270f6cc6227285bb5f
discocorg <david.flaten@gmail.com>
2019-07-17 21:55:24 -0700

    finished Assembler, no symbol support yet

--- 06/HackAssembler.py
+++ 06/HackAssembler.py
@@ -1,4 +1,4 @@
-from Parser import constructor, commandType
+from Parser import constructor, commandType, dest, comp, jump
 
 def main():
     #Get list of lines from file loaded in commandline argument
@@ -12,16 +12,26 @@ def main():
         assemblylines_nocomments.append(line)
     #Remove blank lines.
     assemblylines_nocomments = list(filter(None, assemblylines_nocomments))
+    #convert lines to binary
     for line in assemblylines_nocomments:
         cmdtype =  commandType(line)
         if cmdtype == "A_COMMAND":
             line = int(line[1:])
             binary_number = "{0:015b}".format(line)
-            binary_line = '1' + str(binary_number) 
-            converted_assembly.append(binary_line)
-#        elif cmdtype == "COMMAND":
-
-#    for printme in assemblylines_nocomments:
-#        print(printme)
+            binary_a_line = '0' + str(binary_number) 
+            converted_assembly.append(binary_a_line)
+        elif cmdtype == "C_COMMAND":
+            if "=" in line:
+                d, compjump = line.split("=",1)
+                destline = dest(d)
+            if ";" in line:
+                c, j = line.split(";",1)
+                jumpline = jump(j)
+                binary_c_line = '111' + comp(c) + '000'+ jump(j)
+            else:
+                binary_c_line = '111' + comp(compjump) + destline + jump('null')
+            converted_assembly.append(binary_c_line)
+    for blines in converted_assembly:
+        print(blines)
 if __name__ == "__main__":
     main()
